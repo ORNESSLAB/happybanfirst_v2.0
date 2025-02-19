8b722855ddbb9bad09e21cd62464b03cc34735f0
dehnbekale <randy.bekalemd@gmail.com>
2025-02-19 15:22:05 +0100

    creation de la page creation d'un beneficiaire

--- flask/app.py
+++ flask/app.py
@@ -7,14 +7,16 @@ from werkzeug.utils import secure_filename
 import logging
 import redis
 rd = redis.Redis(host='localhost', port=6379, db=0)
+
 extern = json.loads(rd.get('external_bank_accounts_info'))
 pay_history = json.loads(rd.get('payments_histo'))
 
 logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
-@app.route("/", methods=["GET", "POST"])
 @app.route("/home", methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
+@app.route("/login", methods=["GET", "POST"])
 
 def home():
     if request.method == "POST":
@@ -23,28 +25,29 @@ def home():
         if 'Se connecter' in request.form:
             conn = utils.authentication(user_id=username, password=password)
             app.logger.debug(f'{conn}:   Le test web')
-            return redirect(url_for("login"))
+            return redirect(url_for("operation"))
 
     else:
         return render_template("home.html", error="Invalid credentials")
 
-@app.route("/login", methods=["GET", "POST"])
-def login(user_id=None):
+@app.route("/operation", methods=["GET", "POST"])
+def operation(user_id=None):
     user_id = os.getenv("IB_USERNAME")
     choice = request.form.get("choice")
     if choice == "submit_payment":
         return redirect(url_for("submit_payment"))
     if choice == "check_wallet":
         return redirect(url_for("check_wallet"))
-    return render_template("login.html", user_id=user_id)
+    if choice == "create_bank_account":
+        return redirect(url_for("create_account"))
+    return render_template("choice.html", user_id=user_id)
 
 def get_wallet():
     return render_template("check_wallet.html")
 
 @app.route("/submit_payment", methods=["GET", "POST"])
 def submit_payment():
-    
-    
+    extern.insert(0, {'holderName': '--- Choisissez un compte ---'})
     data = {}
     if request.method == "POST":
         urgency = request.form.get("urgency")
@@ -55,13 +58,18 @@ def submit_payment():
             data['Priorité'] = "24H"
         if urgency == "Différé":
             data['Priorité'] = "48H"
-        
+        recipient = request.form.get("recipient")
+        if recipient == "--- Choisissez un compte ---":
+            flash("Veuillez choisir un compte")
+            data['Bénéficiaire']= ""
+        else:
+            data['Bénéficiaire']= recipient
 
         data['Compte Emetteur'] = request.form.get("source_account")
         data['Commentaire'] = request.form.get("comment")
         data['Libellé']= request.form.get("tag")
         data['Montant']= request.form.get("amount")
-        data['Bénéficiaire']= request.form.get("recipient")
+        
         data['Date désirée']= request.form.get("date")
         
         app.logger.debug(f'{data}:   ')
@@ -87,6 +95,10 @@ def submit_payment():
 
    
     return render_template('submit_payment.html', extern=extern, history=pay_history)
+
+@app.route("/create_exteral_bank_account", methods=["GET", "POST"])
+def create_account():
+    return render_template("create_external.html")
 
 
 
